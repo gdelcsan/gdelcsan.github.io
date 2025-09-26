b5d44591c8bf8d259a11e6b78ebc67a41fd2f406
Gaby <gabrieladelcristo@gmail.com>
2025-09-26 00:46:21 -0400

    Delete src directory

--- src/components/utils/ToolTag.js
+++
@@ -1,8 +0,0 @@
-import { Tag } from '@chakra-ui/react'
-import React from 'react'
-
-export default function ToolTag({ skill }) {
-    return (
-        <Tag colorScheme='teal' fontSize='md' px={4} py={1.5}> {skill}</Tag>
-    )
-}
